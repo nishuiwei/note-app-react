f71f87754c026a30abf792a70e6d7cb528bf024b
卫慧杰 <w920098695@sina.cn>
2024-01-24 17:02:47 +0800

    style: layout setup

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,8 +1,11 @@
+import { Content, RootLayout, Sidebar } from './components'
+
 function App(): JSX.Element {
   return (
-    <div className="flex h-full items-center justify-center">
-      <span className="text-4xl text-blue-500">Hello from ELectron</span>
-    </div>
+    <RootLayout>
+      <Sidebar className="p-2">Sidebar</Sidebar>
+      <Content className="border-l bg-zinc-900/50 border-l-white/20">content</Content>
+    </RootLayout>
   )
 }
 
