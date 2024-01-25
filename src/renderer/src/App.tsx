6efb1bd0aaaed46886425de2d886a6177ea83e81
卫慧杰 <w920098695@sina.cn>
2024-01-25 15:45:59 +0800

    feat: show content & title; toggle content scroll reset

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react'
 import {
   ActionButtonsRow,
   Content,
@@ -10,15 +11,21 @@ import {
 } from './components'
 
 const App = () => {
+  const contentContainerRef = useRef<HTMLDivElement>(null)
+
+  const resetScroll = () => {
+    contentContainerRef.current?.scrollTo(0, 0)
+  }
+
   return (
     <>
       <DraggableTopBar />
       <RootLayout>
         <Sidebar className="p-2">
           <ActionButtonsRow className="flex justify-between mt-1" />
-          <NotePreviewList className="mt-3 sp-y-1" />
+          <NotePreviewList className="mt-3 sp-y-1" onSelect={resetScroll} />
         </Sidebar>
-        <Content className="border-l bg-zinc-900/50 border-l-white/20">
+        <Content ref={contentContainerRef} className="border-l bg-zinc-900/50 border-l-white/20">
           <FloatingNoteTitle className="pt-2" />
           <MarkdownEditor />
         </Content>
---
+++ src/renderer/src/hooks/useMarkdownEditor.tsx
@@ -0,0 +1,9 @@
+import { selectedNoteAtom } from '@renderer/store'
+import { useAtomValue } from 'jotai'
+
+export const useMarkdownEditor = () => {
+  const selectedNote = useAtomValue(selectedNoteAtom)
+  return {
+    selectedNote
+  }
+}
